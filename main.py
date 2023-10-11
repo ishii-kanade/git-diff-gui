a9a6aafc0e8a74739618e85336ef7b130795d4b9
kanade-ishii <112748642+kanade-ishii@users.noreply.github.com>
2023-10-11 09:55:48 +0900

    diffをファイル出力する仕様に変更

--- main.py
+++ main.py
@@ -1,44 +1,72 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import filedialog
 import subprocess
 import os
 
-def select_repository():
-    repo_path = filedialog.askdirectory(title="Select Git Repository")
-    if repo_path:
-        os.chdir(repo_path)
-        load_branches()
+class GitDiffApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-def load_branches():
-    branches = subprocess.getoutput("git branch").split("\n")
-    branches = [branch.strip().replace("* ", "") for branch in branches]
-    combo1['values'] = branches
-    combo2['values'] = branches
+        self.title("Git Diff with Tkinter")
 
-def show_diff():
-    branch1 = combo1.get()
-    branch2 = combo2.get()
-    cmd = f"git diff {branch1}..{branch2}"
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    text.delete(1.0, tk.END)
-    text.insert(tk.END, output.decode())
+        self.configure(bg="white")
 
-app = tk.Tk()
-app.title("Git Diff GUI")
+        self.repo_button = tk.Button(self, text="Select Repository", command=self.select_repository, bg="white", fg="black")
+        self.repo_button.pack(pady=10)
 
-select_repo_button = tk.Button(app, text="Select Repository", command=select_repository)
-select_repo_button.pack(pady=10)
+        self.branch1_var = tk.StringVar(self)
+        self.branch2_var = tk.StringVar(self)
 
-combo1 = ttk.Combobox(app)
-combo2 = ttk.Combobox(app)
-combo1.pack(pady=10)
-combo2.pack(pady=10)
+        self.combo1 = tk.OptionMenu(self, self.branch1_var, "")
+        self.combo1.configure(bg="white", fg="black")
+        self.combo1["menu"].config(bg="white", fg="black")
 
-button = tk.Button(app, text="Diff表示", command=show_diff)
-button.pack(pady=10)
+        self.combo2 = tk.OptionMenu(self, self.branch2_var, "")
+        self.combo2.configure(bg="white", fg="black")
+        self.combo2["menu"].config(bg="white", fg="black")
 
-text = tk.Text(app)
-text.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        self.combo1.pack(pady=5)
+        self.combo2.pack(pady=5)
 
-app.mainloop()
+        self.diff_button = tk.Button(self, text="Export Diff", command=self.export_diff, bg="white", fg="black")
+        self.diff_button.pack(pady=10)
+
+        self.text = tk.Text(self, bg="white", fg="black", insertbackground="black", width=80, height=20)
+        self.text.pack(padx=10, pady=10)
+
+    def select_repository(self):
+        repo_path = filedialog.askdirectory(title="Select Git Repository")
+        if repo_path:
+            os.chdir(repo_path)
+            self.load_branches()
+
+    def load_branches(self):
+        branches = subprocess.getoutput("git branch").split("\n")
+        branches = [branch.strip().replace("* ", "") for branch in branches]
+        self.branch1_var.set("")
+        self.branch2_var.set("")
+        self.combo1["menu"].delete(0, "end")
+        self.combo2["menu"].delete(0, "end")
+        for branch in branches:
+            self.combo1["menu"].add_command(label=branch, command=tk._setit(self.branch1_var, branch))
+            self.combo2["menu"].add_command(label=branch, command=tk._setit(self.branch2_var, branch))
+
+    def export_diff(self):
+        branch1 = self.branch1_var.get()
+        branch2 = self.branch2_var.get()
+        cmd = f"git diff {branch1}..{branch2}"
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+
+        with open("diff_output.txt", "w", encoding="utf-8") as file:
+            if error:
+                file.write("Error:\n" + error.decode())
+            else:
+                file.write(output.decode())
+
+        self.text.insert(tk.END, "Diff has been saved to diff_output.txt")
+
+
+if __name__ == "__main__":
+    app = GitDiffApp()
+    app.mainloop()
