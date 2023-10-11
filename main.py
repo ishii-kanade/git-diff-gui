699030bf6ef469df71b827bef5e962b3ee84ac50
kanade-ishii <112748642+kanade-ishii@users.noreply.github.com>
2023-10-11 11:06:30 +0900

    シンプルな差分も出力できるように変更

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ class GitDiffApp(tk.Tk):
         self.diff_button = tk.Button(self, text="Export Diff", command=self.export_diff, bg="white", fg="black")
         self.diff_button.pack(pady=10)
 
+        simple_diff_btn = tk.Button(self, text="Export Simple Diff", command=self.simple_git_diff)
+        simple_diff_btn.pack()
+
         self.text = tk.Text(self, bg="white", fg="black", insertbackground="black", width=80, height=20)
         self.text.pack(padx=10, pady=10)
 
@@ -50,6 +53,22 @@ class GitDiffApp(tk.Tk):
         for branch in branches:
             self.combo1["menu"].add_command(label=branch, command=tk._setit(self.branch1_var, branch))
             self.combo2["menu"].add_command(label=branch, command=tk._setit(self.branch2_var, branch))
+
+    def simple_git_diff(self):
+        branch1 = self.branch1_var.get()
+        branch2 = self.branch2_var.get()
+        cmd = f"git diff {branch1}..{branch2}"
+
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+
+        with open("simple_diff_output.txt", "w", encoding="utf-8") as file:
+            if error:
+                file.write("Error:\n" + error.decode())
+            else:
+                file.write(output.decode())
+
+        self.text.insert(tk.END, "Simple Git Diff has been saved to simple_diff_output.txt")
 
     def export_diff(self):
         branch1 = self.branch1_var.get()
